23bd0a286d0c742e07a00ac815368d0f3791feb2
Folly FOLLIVI <follivifolly@gmail.com>
2026-07-15 12:37:48 +0000

    Ajout

--- application_web/venv/bin/activate.fish
+++ application_web/venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/folly/Bureau/mesApplis/FasoTour/application_web/venv
+set -gx VIRTUAL_ENV /home/folly/Bureau/mesApplis/fasotour/application_web/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
